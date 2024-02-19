e8586e5768d4a8ddbb54c03c514cfb97dc3968e4
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2024-02-19 02:00:52 -0400

    add Gregtech recipe changes

--- src/main/java/supersymmetry/Supersymmetry.java
+++ src/main/java/supersymmetry/Supersymmetry.java
@@ -23,7 +23,7 @@ import supersymmetry.common.item.SuSyMetaItems;
 import supersymmetry.common.metatileentities.SuSyMetaTileEntities;
 import supersymmetry.loaders.SuSyIRLoader;
 
-@Mod(name = Supersymmetry.NAME, modid = Supersymmetry.MODID, version = Tags.VERSION, dependencies = GTInternalTags.DEP_VERSION_STRING + ";required-after:gcym;required-after:gregtechfoodoption")
+@Mod(name = Supersymmetry.NAME, modid = Supersymmetry.MODID, version = Tags.VERSION, dependencies = GTInternalTags.DEP_VERSION_STRING + ";required-after:gcym;required-after:gregtechfoodoption;after:notreepunching")
 
 public class Supersymmetry {
 
