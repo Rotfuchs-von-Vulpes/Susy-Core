6db356aed4a3e3192631e69d63c0818aaae58d40
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2024-03-17 23:59:33 -0400

    add usunium

--- src/main/java/supersymmetry/common/materials/SuSyElementMaterials.java
+++ src/main/java/supersymmetry/common/materials/SuSyElementMaterials.java
@@ -1,8 +1,23 @@
 package supersymmetry.common.materials;
 
+import gregtech.api.fluids.FluidBuilder;
+import gregtech.api.unification.Element;
+import gregtech.api.unification.Elements;
+import gregtech.api.unification.material.Material;
+import supersymmetry.api.util.SuSyUtility;
+
+import static supersymmetry.common.materials.SusyMaterials.Uwunium;
+
 public class SuSyElementMaterials {
-
+    public static Element Uwu;
     public static void init() {
+        Uwu = Elements.add(0, 1, -1, null, "UwUnium", "Uwu", false);
 
+        Uwunium = new Material.Builder(28000, SuSyUtility.susyId("uwunium"))
+                .ingot()
+                .element(Uwu)
+                .liquid(new FluidBuilder().temperature(1449))
+                .color(0xffa3fb)
+                .build();
     }
 }
